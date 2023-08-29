e4ad27b3f7a55a667c7778d0e5c9888df0b283b6
Daria Zvarich <dorimoriza@gmail.com>
2023-08-29 19:43:44 +0100

    start project

---
+++ app.js
@@ -0,0 +1,69 @@
+ // Variables 
+
+ let btn = document.querySelector('#new-quote');
+ let quote = document.querySelector('.quote');
+ let person = document.querySelector('.person');
+
+ const quotes = [{
+
+        quote: "The only way to do great work is to love what you do.",
+        person:  "Steve Jobs"
+    },
+    {
+        quote: "In three words I can sum up everything I've learned about life: it goes on.",
+        person: "Robert Frost"
+    },
+    {
+        quote: "The future belongs to those who believe in the beauty of their dreams.",
+        person: "Eleanor Roosevelt"
+    },
+    {
+        quote: "The only limit to our realization of tomorrow will be our doubts of today.",
+        person: "Franklin D. Roosevelt"
+    },
+    {
+        quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+        person: "Winston Churchill"
+    },
+    {
+        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        person: "Nelson Mandela"
+    },
+    {
+        quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+        person: "Ralph Waldo Emerson"
+    },
+    {
+        quote: "The best time to plant a tree was 20 years ago. The second best time is now.", 
+        person: "Chinese Proverb"
+    },
+    {
+        quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        person: "Albert Einstein"
+    },
+    {
+        quote: "The only thing we have to fear is fear itself.",
+        person: "Franklin D. Roosevelt"
+    },
+    {
+        quote: "Life is really simple, but we insist on making it complicated.",
+        person: "Confucius"
+    },
+    {
+        quote: "Believe you can and you're halfway there.", 
+        person: "Theodore Roosevelt"
+    },
+    {
+        quote: "I have not failed. I've just found 10,000 ways that won't work.",
+        person: "Thomas A. Edison"
+    },
+    {
+        quote: "It does not matter how slowly you go as long as you do not stop.",
+        person: "Confucius"
+    },
+    {
+        quote: "The secret of getting ahead is getting started.",
+        person: "Mark Twain"
+    },
+    
+ ]
